65043f49e7257ceb34e54005a157000238b01fce
Suzoka/Morgan <mor9an77@gmail.com>
2023-10-24 12:33:45 +0200

    :construction: Work on the graphs

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 fetch('data.json').then(function (response) {
     response.json().then(function (data) {
-        data.forEach((element, iAnnee)=> {
-            let gagnants=[];
+        let gagnants = [];
+        data.forEach((element, iAnnee) => {
+            let gagnant = [];
             element.result.forEach((univ) => {
-                if (univ.charts[0] == element.winner[0]){
-                    gagnants.push(univ);
+                if (univ.charts[0] == element.winner[0]) {
+                    gagnant.push(univ);
                 }
             })
-            console.log(gagnants);
-            
-        });
+            gagnants.push(gagnant);
+        }); 
+        console.log(gagnants);
+        const svg = d3.select('svg');
+        svg.style('background-color', 'rgba(110, 98, 157, 0.55)');
+
+        svg.selectAll('g')
+            .data(gagnants)
+            .join('g')
+            .attr('id', (dataG, i) => 2015 + i)
+            .attr('transform', (dataG, i) => `translate(${(5 + (300 / 9) * i)}, 0), scale(1, -1)`)
+                .selectAll('g')
+                .data (dataG => dataG)
+                .join('g')
+                .attr('transform', (dataU, i, dataG) => `translate(${((300/9 - 5)/dataG.length) * i}, 0)`)
+                    .append('rect')
+                    .attr('width', (dataU, i, dataG) => (300/9 - 5)/dataG.length)
+                    .attr('height', dataU => dataU.charts[0]*35)
+                    .attr('fill', dataU => dataU.color)
+
+  
+
+
     });
 });
