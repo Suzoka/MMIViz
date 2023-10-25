ae6ffa4d1f0e4eeebc775716df68353cd36cca5c
Suzoka/Morgan <mor9an77@gmail.com>
2023-10-25 10:34:06 +0200

    :sparkles functionnal first graph

--- script.js
+++ script.js
@@ -9,27 +9,41 @@ fetch('data.json').then(function (response) {
                 }
             })
             gagnants.push(gagnant);
-        }); 
+        });
         console.log(gagnants);
-        const svg = d3.select('svg');
-        svg.style('background-color', 'rgba(110, 98, 157, 0.55)');
+        d3.select('svg').style('background-color', 'rgba(110, 98, 157, 0.55)');
 
-        svg.selectAll('g')
+        const barre = d3.select('g.graph').selectAll('g')
             .data(gagnants)
             .join('g')
             .attr('id', (dataG, i) => 2015 + i)
             .attr('transform', (dataG, i) => `translate(${(5 + (300 / 9) * i)}, 0), scale(1, -1)`)
-                .selectAll('g')
-                .data (dataG => dataG)
-                .join('g')
-                .attr('transform', (dataU, i, dataG) => `translate(${((300/9 - 5)/dataG.length) * i}, 0)`)
-                    .append('rect')
-                    .attr('width', (dataU, i, dataG) => (300/9 - 5)/dataG.length)
-                    .attr('height', dataU => dataU.charts[0]*35)
-                    .attr('fill', dataU => dataU.color)
+            .selectAll('g')
+            .data(dataG => dataG)
+            .join('g')
+            .attr('class', dataU => dataU.university.replaceAll(' ', '').toLowerCase())
+            .attr('transform', (dataU, i, dataG) => `translate(${((300 / 9 - 5) / dataG.length) * i}, 0)`);
 
-  
 
+        barre.append('rect')
+            .attr('width', (dataU, i, dataG) => (300 / 9 - 5) / dataG.length)
+            .attr('height', dataU => dataU.charts[0] * (160 / 3))
+            .attr('fill', dataU => dataU.color);
+
+        barre.append('circle')
+            .attr('cx', (dataU, i, dataG) => (300 / 9 - 5) / dataG.length / 2)
+            .attr('cy', dataU => dataU.charts[0] * (160 / 3))
+            .attr('r', (dataU, i, dataG) => (300 / 9 - 5) / dataG.length / 2)
+            .attr('fill', 'white');
+
+        barre.on('mouseenter', function (event, dataU) {
+            barre.style('opacity', 0.2);
+            d3.selectAll("g." + dataU.university.replaceAll(' ', '').toLowerCase()).style('opacity', 1);
+        });
+
+        barre.on('mouseleave', function (event, dataU) {
+            barre.style('opacity', 1);
+        });
 
     });
 });
