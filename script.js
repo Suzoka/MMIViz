eaeca68f5a2dcc112bd7a0167af6428676c0c6b5
Suzoka/Morgan <mor9an77@gmail.com>
2023-11-17 13:56:25 +0100

    :bulb: start comment code

--- script.js
+++ script.js
@@ -1,94 +1,127 @@
+//Défnition de deux variables globales
 let fn = null;
 let podiumFlag = false;
 
+//Récupération des données
 fetch('data.json').then(function (response) {
     response.json().then(function (data) {
+        //Fonction pour le PopUp si la définition de l'écran est inférieur à 1024px
         popup();
 
+        //Séléctionner uniquement les données voulues
         let gagnants = getGagnants(data, document.querySelector('select[name="trophy"]').value);
+
+        //Création du graphique
         graphique(gagnants);
+
+        //Création de la légende
         legende(data);
 
+        //Si nous changons le graph à afficher
         document.querySelector('select[name="trophy"]').addEventListener('change', function (event) {
+
+            //Récupération des données voulues
             gagnants = getGagnants(data, event.target.value);
+
+            //Suppression de l'ancien graphique
             d3.select('g.graph').selectAll('g').selectAll('circle').remove()
             d3.select('g.graph').selectAll('g').selectAll("rect").transition().duration(300).attr('height', 0).remove();
             setTimeout(function () {
                 d3.select('g.graph').selectAll('g').remove();
                 d3.select('g.valeurs').selectAll('g').remove();
+                //tracé du nouveau graphique
                 graphique(gagnants);
             }, 300);
         });
     });
 });
 
-
+//Fonction pour récupérer les données voulues
 function getGagnants(data, cas) {
     let gagnants = [];
     let valeur;
+    //Pour chaque année
     data.forEach(element => {
         let gagnant = [];
+        //Pour chaque université
         element.result.forEach((univ) => {
             valeur = 0;
+            //Si nous voulons toutes les places
             if (cas == 1) {
                 valeur = univ.charts[0] + univ.charts[1] + univ.charts[2];
             }
+            //Si nous voulons les premières places
             else {
                 valeur = univ.charts[0];
             }
+            //Si le nombre de trophées est égal au nombre de trophés remportés par le gagnant de l'année
             if (valeur == element.winner[cas]) {
+                //Mettre le nombre de trophée qui sera exploité à la case 3 du tableau
                 univ.charts[3] = valeur;
+                //Ajouter l'université au tableau des gagnants de l'année
                 gagnant.push(univ);
             }
         })
+        //Ajouter le tableau des gagnants de l'année au tableau des gagnants
         gagnants.push(gagnant);
     });
+    //Retourner le tableau des gagnants
     return gagnants;
 }
 
+//Fonction pour créer le graphique
 function graphique(gagnants) {
+    //Connaître l'échelle du graphique
     const cas = document.querySelector('select[name="trophy"]').value;
     const echelle = { valeurs: [[1, 2, 3], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]] };
 
+    //création d'un groupe par année
     const barre = d3.select('g.graph').selectAll('g')
         .data(gagnants)
         .join('g')
         .attr('id', (dataG, i) => 2015 + i)
         .attr('transform', (dataG, i) => `translate(${(5 + (300 / 9) * i)}, 0), scale(1, -1)`)
         .selectAll('g')
+        //création d'un groupe par université
         .data(dataG => dataG)
         .join('g')
         .attr('class', dataU => dataU.university.replaceAll(' ', '').replaceAll('é', 'e').replaceAll('ê', 'e').replaceAll('ô', 'o').toLowerCase())
         .attr('transform', (dataU, i, dataG) => `translate(${((300 / 9 - 5) / dataG.length) * i}, 0)`);
 
+    //création d'une barre par université, avec une épaisseur partagée en fonction du nombre de gagnants et une hauteur de 0
     barre.append('rect')
         .attr('width', (dataU, i, dataG) => (300 / 9 - 5) / dataG.length)
         .attr('height', (dataU, i, dataG) => (300 / 9 - 5) / dataG.length / 2)
         .attr('fill', dataU => dataU.color)
 
+    //Création du cercle contenant les logos des universités
     barre.append('circle')
         .attr('cx', (dataU, i, dataG) => (300 / 9 - 5) / dataG.length / 2)
         .attr('cy', (dataU, i, dataG) => (300 / 9 - 5) / dataG.length / 2)
         .attr('r', (dataU, i, dataG) => (300 / 9 - 5) / dataG.length / 2)
         .attr('fill', 'white')
 
+    //Animation des barres jusqu'à la bonne valeur
     setTimeout(function () {
         barre.select('rect').transition().duration(600).attr('height', dataU => dataU.charts[3] * (160 / echelle.valeurs[cas].length))
         barre.select("circle").transition().duration(600).attr('cy', dataU => dataU.charts[3] * (160 / echelle.valeurs[cas].length))
         setTimeout(function () { podiumFlag = false; updateTitre();}, 600)
     }, 10);
 
+    //Au survole des barres, rendre les barres des autres université que celle survolée transparentes et afficher une infobulle
     barre.on('mouseenter', function (event, dataU) {
         barre.transition().duration(500).style('opacity', 0.2);
         d3.selectAll("g." + dataU.university.replaceAll(' ', '').replaceAll('é', 'e').replaceAll('ê', 'e').replaceAll('ô', 'o').toLowerCase()).transition().duration(300).style('opacity', 1);
         infoBulle(event, dataU);
     });
 
+    //Rétablir l'opacité des barres et supprimer l'infobulle
     barre.on('mouseleave', function (event, dataU) {
         barre.transition().duration(500).style('opacity', 1);
         infoBulle();
     });
 
+    //Ajout des années sur l'axe des abscisses
     d3.select('svg')
         .selectAll('g.graph>g')
         .append('text')
@@ -97,11 +130,13 @@ function graphique(gagnants) {
         .attr('y', 12.5)
         .attr('x', 2.25)
 
+    //Création d'un groupe pour chacune des valeurs de l'axe des ordonnées
     const valeur = d3.select('g.valeurs')
         .selectAll('g')
         .data(echelle.valeurs[cas])
         .join('g')
 
+    //Ajout des lignes sur l'axe des ordonnées
     valeur.append('line')
         .attr('x1', -5)
         .attr('x2', 300)
@@ -112,6 +147,7 @@ function graphique(gagnants) {
         .attr('stroke-dasharray', '5 5')
         .attr('transform', 'scale(1, -1)')
 
+    //Ajout des valeurs sur l'axe des ordonnées
     valeur.append('text')
         .text(data => data)
         .attr('y', data => (data * (160 / echelle.valeurs[cas].length) - 5) * -1)
@@ -119,13 +155,18 @@ function graphique(gagnants) {
         .style('text-anchor', 'middle')
 }
 
+//Création de la légende
 function legende(data) {
+    //Pour chaque université
     data[0].result.forEach((univ) => {
+        //Ajouter un paragraphe avec le nom de l'université et la couleur de l'université dans une variable CSS
         document.querySelector('section.universite').innerHTML += `<p class="${univ.university.replaceAll(' ', '').replaceAll('é', 'e').replaceAll('ê', 'e').replaceAll('ô', 'o').toLowerCase()}" style="  --color: ${univ.color};">${univ.university}</p>`;
     });
+    //Ajouter les évènements sur les paragraphes
     eventLegende(data);
 }
 
+//Ajout des évènements sur les paragraphes de la légende
 function eventLegende(donnees) {
     document.querySelectorAll('section.universite p').forEach((univ) => {
 
